deb74a51b0824fa0ab0f415c713d83ee2a05008e
Abdelrahman Saeed Elhassan <abdosaeedelhassan@gmail.com>
2022-09-16 19:11:32 +0200

    first-commit

--- dist/library.js
+++ dist/library.js
@@ -3,19 +3,62 @@
 var vue = require('vue');
 
 var script = {
-  name: "InputText",
+  name: "InputField",
+  props: {
+    modelvalue: {
+      default: null,
+    },
+    type: {
+      default: "text",
+    },
+    errorMessage: {
+      default: null,
+    },
+    inputAppendClass: {
+      default: "",
+    },
+    errorAppendClass: {
+      default: "",
+    },
+  },
+  setup() {
+    let inputFiled = vue.ref(props.modelvalue);
+    return {
+      inputFiled,
+    };
+  },
+  watch: {
+    inputFiled: function (newVal) {
+      this.$emit("update:modelVal", newVal);
+    },
+  },
 };
 
-const _hoisted_1 = { type: "text" };
+const _hoisted_1 = ["type"];
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (vue.openBlock(), vue.createElementBlock("input", _hoisted_1))
+  return (vue.openBlock(), vue.createElementBlock(vue.Fragment, null, [
+    vue.withDirectives(vue.createElementVNode("input", {
+      type: $props.type,
+      "onUpdate:modelValue": _cache[0] || (_cache[0] = $event => (($setup.inputFiled) = $event)),
+      class: vue.normalizeClass($props.inputAppendClass)
+    }, null, 10 /* CLASS, PROPS */, _hoisted_1), [
+      [vue.vModelDynamic, $setup.inputFiled]
+    ]),
+    ($props.errorMessage)
+      ? (vue.openBlock(), vue.createElementBlock("span", {
+          key: 0,
+          style: {"color":"red"},
+          class: vue.normalizeClass($props.errorAppendClass)
+        }, vue.toDisplayString($props.errorMessage), 3 /* TEXT, CLASS */))
+      : vue.createCommentVNode("v-if", true)
+  ], 64 /* STABLE_FRAGMENT */))
 }
 
 script.render = render;
-script.__file = "src/InputText.vue";
+script.__file = "src/InputField.vue";
 
-var components = { InputText: script };
+var components = { InputField: script };
 
 const plugin = {
     install(Vue) {
--- dist/library.mjs
+++ dist/library.mjs
@@ -1,19 +1,62 @@
-import { openBlock, createElementBlock } from 'vue';
+import { ref, openBlock, createElementBlock, Fragment, withDirectives, createElementVNode, normalizeClass, vModelDynamic, toDisplayString, createCommentVNode } from 'vue';
 
 var script = {
-  name: "InputText",
+  name: "InputField",
+  props: {
+    modelvalue: {
+      default: null,
+    },
+    type: {
+      default: "text",
+    },
+    errorMessage: {
+      default: null,
+    },
+    inputAppendClass: {
+      default: "",
+    },
+    errorAppendClass: {
+      default: "",
+    },
+  },
+  setup() {
+    let inputFiled = ref(props.modelvalue);
+    return {
+      inputFiled,
+    };
+  },
+  watch: {
+    inputFiled: function (newVal) {
+      this.$emit("update:modelVal", newVal);
+    },
+  },
 };
 
-const _hoisted_1 = { type: "text" };
+const _hoisted_1 = ["type"];
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (openBlock(), createElementBlock("input", _hoisted_1))
+  return (openBlock(), createElementBlock(Fragment, null, [
+    withDirectives(createElementVNode("input", {
+      type: $props.type,
+      "onUpdate:modelValue": _cache[0] || (_cache[0] = $event => (($setup.inputFiled) = $event)),
+      class: normalizeClass($props.inputAppendClass)
+    }, null, 10 /* CLASS, PROPS */, _hoisted_1), [
+      [vModelDynamic, $setup.inputFiled]
+    ]),
+    ($props.errorMessage)
+      ? (openBlock(), createElementBlock("span", {
+          key: 0,
+          style: {"color":"red"},
+          class: normalizeClass($props.errorAppendClass)
+        }, toDisplayString($props.errorMessage), 3 /* TEXT, CLASS */))
+      : createCommentVNode("v-if", true)
+  ], 64 /* STABLE_FRAGMENT */))
 }
 
 script.render = render;
-script.__file = "src/InputText.vue";
+script.__file = "src/InputField.vue";
 
-var components = { InputText: script };
+var components = { InputField: script };
 
 const plugin = {
     install(Vue) {
